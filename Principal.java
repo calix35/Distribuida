867b0d26e070ba36bc34cad2f300acc85ab5d7ed
Alan Diaz <calix35@gmail.com>
2021-08-30 18:54:09 -0500

    Se agrego la clase Principal

---
+++ Principal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;//Objeto para leer datos del teclado
+
+public class Principal{
+    public static void main(String[] args){
+
+        Scanner sc =  new Scanner(System.in);
+        String nombre;
+        String apellidos;
+        int edad;
+
+        System.out.print("Nombre: ");
+        nombre = sc.nextLine();
+        System.out.print("Apellidos: ");
+        apellidos = sc.nextLine();
+        System.out.print("Edad: ");
+        edad = sc.nextInt();
+
+        /*
+            String - nextLine()
+            int - nextInt()
+            float - nextFloat()
+            double - nextDouble()
+        */
+
+        Usuario nuevo = new Usuario(nombre, apellidos, edad);
+
+        System.out.println(nuevo);
+
+
+    }
+}
